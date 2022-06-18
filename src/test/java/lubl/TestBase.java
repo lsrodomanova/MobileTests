f366a8a960bc9e46af448c84d5bee160a995b043
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-06-18 17:07:33 +0300

    правки

--- src/test/java/lubl/TestBase.java
+++ src/test/java/lubl/TestBase.java
@@ -4,13 +4,11 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import drivers.BrowserstackMobileDriver;
 import helpers.AllureAttachments;
-import helpers.AllureAttachments;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 
-import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AllureAttachments.sessionId;
--- src/test/java/lubl/WikiTests.java
+++ src/test/java/lubl/WikiTests.java
@@ -1,10 +1,7 @@
 package lubl;
-import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Condition;
 import io.appium.java_client.AppiumBy;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
