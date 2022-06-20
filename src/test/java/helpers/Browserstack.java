74e39ac1783e6c72cde4371f745dfb418d0b37e4
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-06-20 09:45:31 +0300

    правки

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -1,19 +1,11 @@
 package helpers;
 
-import config.RemoteConfig;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import org.aeonbits.owner.ConfigFactory;
-
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
 
 public class Browserstack {
 
-    public static ExtractableResponse<Response> videoUrl(String sessionId) {
-        /*RemoteConfig remoteConfig = ConfigFactory.create(RemoteConfig.class, System.getProperties());
-        String login = remoteConfig.login();
-        String password = remoteConfig.key();*/
+    public static String videoUrl(String sessionId) {
         String url = format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
 
         return given()
